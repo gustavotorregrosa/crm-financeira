5b19f96d1f6c59692da8ef4cc37b45a3799fc407
Gustavo Torregrosa <gustavo@visie.com.br>
2019-04-10 06:51:10 -0300

    partial

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -1,1 +1,9 @@
-ola mundo admin
+@extends('layout-geral.layout')
+
+@section('titulo', 'Painel Admin')
+
+@section('barra-lateral')
+    @component('administrador.barra-lateral')
+
+    @endcomponent
+@endsection
---
+++ resources/views/layout-geral/layout.blade.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+ 
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('custom-assets/painel-geral/assets/vendor/bootstrap/css/bootstrap.min.css')}}">
+    <link href="{{asset('custom-assets/painel-geral/assets/vendor/fonts/circular-std/style.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('custom-assets/painel-geral/assets/libs/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('custom-assets/painel-geral/assets/vendor/fonts/fontawesome/css/fontawesome-all.css')}}">
+
+    <title>@yield('titulo', 'CRM Financeira - Admin')</title>
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- main wrapper -->
+    <!-- ============================================================== -->
+    <div class="dashboard-main-wrapper">
+        <!-- ============================================================== -->
+        <!-- navbar -->
+        <!-- ============================================================== -->
+        <div class="dashboard-header">
+            <nav class="navbar navbar-expand-lg bg-white fixed-top">
+                <a class="navbar-brand" href="index.html">{{ env('APP_NAME')}}</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto navbar-right-top">
+                        <li style="padding: 1.5em;"><p>{{ Auth::user()->name }}</p></li>
+                        <li style="margin: 1em;"><button class="btn btn-success btn-sm">Logout</button></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <!-- ============================================================== -->
+        <!-- end navbar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- left sidebar -->
+        <!-- ============================================================== -->
+      
+
+        @yield('barra-lateral')
+        <!-- ============================================================== -->
+        <!-- end left sidebar -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- wrapper  -->
+        <!-- ============================================================== -->
+        <div class="dashboard-wrapper">
+            <div class="dashboard-ecommerce">
+                <div class="container-fluid dashboard-content ">
+                    <!-- ============================================================== -->
+                    <!-- pageheader  -->
+                    <!-- ============================================================== -->
+                    <div class="row">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                            <div class="page-header">
+                                <h2 class="pageheader-title">E-commerce Dashboard Template </h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- ============================================================== -->
+                    <!-- end pageheader  -->
+                    <!-- ============================================================== -->
+                    <div class="ecommerce-widget">
+
+                      
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- ============================================================== -->
+        <!-- end wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- end main wrapper  -->
+    <!-- ============================================================== -->
+    <!-- Optional JavaScript -->
+    <!-- jquery 3.3.1 -->
+    <script src="{{asset('custom-assets/painel-geral/assets/vendor/jquery/jquery-3.3.1.min.js')}}"></script>
+    <!-- bootstap bundle js -->
+    <script src="{{asset('custom-assets/painel-geral/assets/vendor/bootstrap/js/bootstrap.bundle.js')}}"></script>
+    <!-- slimscroll js -->
+    <script src="{{asset('custom-assets/painel-geral/assets/vendor/slimscroll/jquery.slimscroll.js')}}"></script>
+    <!-- main js -->
+    <script src="{{asset('custom-assets/painel-geral/assets/libs/js/main-js.js')}}"></script>
+    <!-- chart chartist js -->
+    
+    <!-- sparkline js -->
+    <script src="{{asset('custom-assets/painel-geral/assets/vendor/charts/sparkline/jquery.sparkline.js')}}"></script>
+
+</body>
+ 
+</html>
